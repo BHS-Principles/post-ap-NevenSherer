bb29ff51d084c71b632c0aa65c109037b9f6a6f8
nevensherer <2007055@apps.nsd.org>
2025-05-23 09:26:42 -0700

    Page names
    Sets the page names based on navbar name

--- project/js/top-bar.js
+++ project/js/top-bar.js
@@ -2,7 +2,7 @@ function create_top_bar(page_name) {
     var template = `
         <div id="banner" class="banner">
             <a id="home" href="../information/main.html">HOME</a>
-            <a id="game-list" href="../information/game-list.html">GAMES</a>
+            <a id="games" href="../information/game-list.html">GAMES</a>
             <div id="banner-right" class="banner-right">
                 <a id="upcoming" href="../information/upcoming.html">UPCOMING</a>
                 <a id="about" href="../information/about.html">ABOUT</a>
@@ -12,4 +12,9 @@ function create_top_bar(page_name) {
 
     document.getElementById("title-bar").innerHTML = template;
     document.getElementById(page_name).classList.add("active");
+    console.log(page_name.charAt(0).toUpperCase());
+    page_name_temp = page_name.slice(1, page_name.length);
+    page_name = page_name.charAt(0).toUpperCase();
+    page_name = page_name + page_name_temp;
+    document.title = page_name;
 }
