d5f30a3fd8771e056e07682ef7a8c42b1264432d
nevensherer <2007055@apps.nsd.org>
2025-05-27 10:05:23 -0700

    Wordle implementation + page names
    Adds the wordle project that I have already made with the addition of the navbar standard on the page.
    
    Also adds functionality so there is the ability to highlight one navbar section with a different page name

--- project/js/top-bar.js
+++ project/js/top-bar.js
@@ -1,6 +1,41 @@
-function create_top_bar(page_name) {
+function create_top_bar(data_value) {
     var template = `
         <div id="banner" class="banner">
+            <style>
+                body {
+                    margin: 0;
+                    font-family: Arial, Helvetica, sans-serif;
+                }
+
+                .banner {
+                    overflow: hidden;
+                    background-color: #0a0a0a;
+                }
+
+                .banner a {
+                    float: left;
+                    text-align: center;
+                    color: #f0f0f0;
+                    padding: 14px 16px;
+                    text-decoration: none;
+                    font-size: 17px;
+                }
+
+                .banner a:hover {
+                    background-color: #f0f0f0;
+                    color: #0a0a0a;
+                    cursor: pointer;
+                }
+
+                .banner a.active {
+                    background-color: rgb(40, 160, 255);
+                    color: #f0f0f0;
+                }
+
+                .banner-right {
+                    float: right;
+                }
+            </style>
             <a id="home" href="../information/main.html">HOME</a>
             <a id="games" href="../information/game-list.html">GAMES</a>
             <div id="banner-right" class="banner-right">
@@ -10,11 +45,12 @@ function create_top_bar(page_name) {
         </div>
     `;
 
+    data_values = data_value.split(", ");
+
+    highlight_page = data_values[0];
+    page_name = data_values[1];
+
     document.getElementById("title-bar").innerHTML = template;
-    document.getElementById(page_name).classList.add("active");
-    console.log(page_name.charAt(0).toUpperCase());
-    page_name_temp = page_name.slice(1, page_name.length);
-    page_name = page_name.charAt(0).toUpperCase();
-    page_name = page_name + page_name_temp;
+    document.getElementById(highlight_page).classList.add("active");
     document.title = page_name;
 }
